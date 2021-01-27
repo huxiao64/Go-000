2fafd42ef9372f248959819d1382f6abbb7439f8
huxiao64 <huxiao64@me.com>
2021-01-27 23:46:41 +0800

    week09 homework

---
+++ Week09/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+	"strconv"
+	"bufio"
+)
+
+var (
+	globalID int
+	idLocker sync.Mutex
+)
+
+func GenUserID() int {
+	idLocker.Lock()
+	defer idLocker.Unlock()
+
+	globalID++
+	return globalID
+}
+
+type User struct {
+	ID int
+	Addr string
+	EnterAt time.Time
+	MessageChannel chan string
+}
+
+func sendMessage(conn net.Conn, ch<-chan string) {
+	for msg:= range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	user := &User{
+		ID:	GenUserID(),
+		Addr:	conn.RemoteAddr().String(),
+		EnterAt:	time.Now(),
+		MessageChannel:	make(chan string, 8),
+	}
+
+	go sendMessage(conn, user.MessageChannel)
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		fmt.Println(strconv.Itoa(user.ID) + ":" + input.Text())
+		user.MessageChannel <- input.Text()
+	}
+
+	if err := input.Err(); err!=nil {
+		log.Println("error:", err)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":88")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
